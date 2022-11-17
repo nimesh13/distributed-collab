be93e48c1ce0db56c82da33d78b9cd248c46fc14
Ivan Hernandez <hernandezivancs@gmail.com>
2022-11-16 16:16:12 -0800

    DC-json Updated local file type and POST writing to file

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from crypt import methods
 from flask import Flask, json, request
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
-filename = "hello.txt"
+filename = "hello.json"
 
 app = Flask(__name__)
 
-@app.route("/health")
+@app.route('/health/')
 def healthCheck():
     return "Alive", 200
 
-@app.route("/companies")
+@app.route('/companies')
 def home():
     return json.dumps(companies)
 
@@ -20,15 +19,14 @@ def add_income():
     companies.append(request.get_json())
     return '', 200
 
-@app.route("/file", methods=["POST"])
+@app.route("/file", methods=['GET', 'POST'])
 def file():
     if request.is_json:
         rqst = request.get_json()
-        file_update = rqst["content"]
         with open(filename, "a") as myfile:
-            myfile.write(file_update)
+            json.dump(rqst, myfile)
         return "OK", 200
     return {"error": "Request must be JSON"}, 415
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
