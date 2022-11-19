425abe196a5a47b9a0abca86c186d51614e86535
nimesh13ad <nimesh13ad@gmail.com>
2022-11-18 17:56:38 -0800

    cleaning main file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, json, request
-import requests
+from controller import app
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -8,29 +7,6 @@ parser.add_argument(dest='port', help="Port number where this server starts up a
 # Parse and print the results
 args, _ = parser.parse_known_args()
 port = args.port
-
-FILENAME = "hello.txt"
-NODE_URL = "http://127.0.0.1:8002"
-
-app = Flask(__name__)
-
-@app.route("/health")
-def healthCheck():
-    return "Alive", 200
-
-@app.route("/query")
-def queryNode():
-    return requests.get(NODE_URL + "/health").content
-
-@app.route("/file", methods=["POST"])
-def file():
-    if request.is_json:
-        rqst = request.get_json()
-        file_update = rqst["content"]
-        with open(FILENAME, "a") as myfile:
-            myfile.write(file_update)
-        return "OK", 200
-    return {"error": "Request must be JSON"}, 415
-    
+ 
 if __name__ == "__main__":
     app.run(debug=True, port=port)
