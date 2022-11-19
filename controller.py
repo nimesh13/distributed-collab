23fba8048d5e0f418a8b7f5b226bb9e0d4519952
Kevin Prakash <kevprakash@gmail.com>
2022-11-19 13:57:10 -0800

    Added message passing and refactored node initialization

--- controller.py
+++ controller.py
@@ -4,22 +4,51 @@ import requests
 FILENAME = "hello.txt"
 NODE_URL = "http://127.0.0.1:8002"
 
+# Tracking variables for gossip
+NEIGHBOURS = []
+RECEIVED_MESSAGE_IDS = []
+
 app = Flask(__name__)
+
 
 @app.route("/health")
 def healthCheck():
     return "Alive", 200
 
+
 @app.route("/query")
 def queryNode():
     return requests.get(NODE_URL + "/health").content
+
 
 @app.route("/file", methods=["POST"])
 def file():
     if request.is_json:
         rqst = request.get_json()
         file_update = rqst["content"]
-        with open(FILENAME, "a") as myfile:
-            myfile.write(file_update)
-        return "OK", 200
+        update_id = rqst["id"]
+        if update_id in RECEIVED_MESSAGE_IDS:
+            return "OK", 200
+        else:
+            RECEIVED_MESSAGE_IDS.append(update_id)
+            with open(FILENAME, "a") as myfile:
+                myfile.write(file_update)
+            forwardMessage("/file", rqst)
+            return "OK", 200
     return {"error": "Request must be JSON"}, 415
+
+
+# Function for forwarding a message to all neighbours
+# Basis of gossip protocol
+# Should only be called if the node has not previously received the message before
+def forwardMessage(command, json):
+    for neighbour in NEIGHBOURS:
+        requests.post(neighbour + command, json=json)
+
+
+# Function for adding neighbours
+# Has a check against adding an already existing neighbour
+def addNeighbour(IP, port):
+    neighbour_string = "http://" + str(IP) + ":" + str(port)
+    if neighbour_string not in NEIGHBOURS:
+        NEIGHBOURS.append(neighbour_string)
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import controller
 from controller import app
 import argparse
 
+# Parse initialization parameters
 parser = argparse.ArgumentParser()
-parser.add_argument(dest='port', help="Port number where this server starts up at.")
+parser.add_argument('--host', default="127.0.0.1", dest='host', help='Host IP address where this server starts up at.')
+parser.add_argument('--port', default="8002", dest='port', help="Port number where this server starts up at.")
 
-# Parse and print the results
 args, _ = parser.parse_known_args()
 port = args.port
+host = args.host
  
 if __name__ == "__main__":
-    app.run(debug=True, port=port)
+    controller.NODE_URL = "http://" + str(host) + ":" + str(port)   # Makes the node URL consistent
+    app.run(debug=True, host=host, port=port)
