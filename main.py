9226ee4200f458e482131983abacd7639ee76ef8
nimesh13ad <nimesh13ad@gmail.com>
2022-11-21 20:45:07 -0800

    updated default host addr

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from time import time
 
 # Parse initialization parameters
 parser = argparse.ArgumentParser()
-parser.add_argument('--host', default="127.0.0.1", dest='host', help='Host IP address where this server starts up at.')
+parser.add_argument('--host', default="0.0.0.0", dest='host', help='Host IP address where this server starts up at.')
 parser.add_argument('--port', default="8002", dest='port', help="Port number where this server starts up at.")
 
 args, _ = parser.parse_known_args()
