8cd83f063dc7bb740c0c76bddafb31f926e0c19e
nimesh13ad <nimesh13ad@gmail.com>
2022-11-24 18:30:04 -0800

    use ts as HLC

--- lww.py
+++ lww.py
@@ -1,38 +1,43 @@
+from hlc import HLC
+
 class LWW:
     def __init__(self) -> None:
         self.add = {}
         self.remove = {}
     
-    def addSet(self, element, ts) -> bool:
+    def querySet(self, set, element: str) -> bool:
+        return element in set
+    
+    def addSet(self, element: str, ts: HLC) -> bool:
         operation = False
-        present_in_add_set = element in self.add
-        present_in_remove_set = element in self.remove
+        present_in_add_set = self.querySet(self.add, element)
+        present_in_remove_set = self.querySet(self.remove, element)
 
         if not present_in_add_set and not present_in_remove_set:
             self.add[element] = ts
             operation = True
-        elif present_in_add_set and ts > self.add[element]:
+        elif present_in_add_set and self.add[element].cmpr(ts) < 0:
             self.add[element] = ts
             operation = True
-        elif present_in_remove_set and ts > self.remove[element]:
+        elif present_in_remove_set and self.remove[element].cmpr(ts) < 0:
             del self.remove[element]
             self.add[element] = ts
             operation = True
 
         return operation
 
-    def removeSet(self, element, ts) -> bool:
+    def removeSet(self, element: str, ts: HLC) -> bool:
         operation = False
-        present_in_add_set = element in self.add
-        present_in_remove_set = element in self.remove
+        present_in_add_set = self.querySet(self.add, element)
+        present_in_remove_set = self.querySet(self.remove, element)
 
         if not present_in_add_set and not present_in_remove_set:
             self.remove[element] = ts
             operation = True
-        elif present_in_remove_set and ts > self.remove[element]:
+        elif present_in_remove_set and self.remove[element].cmpr(ts) < 0:
             self.remove[element] = ts
             operation = True
-        elif present_in_add_set and ts > self.add[element]:
+        elif present_in_add_set and self.add[element].cmpr(ts) < 0:
             del self.add[element]
             self.remove[element] = ts
             operation = True
