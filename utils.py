66a7068b4d098bebea5f0763cea07c50e47948fa
nimesh13ad <nimesh13ad@gmail.com>
2022-11-25 23:31:42 -0800

    fn to gen unique msg id

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
-def getUniqueId(event):
+from uuid import uuid4
+
+def getEventId(event):
     return event['day'] + '-' + event['title']
 
 def getEvent(uniqueId: str):
     day, title = uniqueId.split('-')
     return {'day': day, 'title': title}
+
+def genUniqueId():
+    return str(uuid4())
