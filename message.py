f3e71568d8d07af86107dd061b99f7f07aa5ceb6
nimesh13ad <nimesh13ad@gmail.com>
2022-11-26 18:28:17 -0800

    fix imports and type

--- message.py
+++ message.py
@@ -1,10 +1,8 @@
-from ast import dump
-from hlc import HLC
 from json import dumps
 
 class Message:
-    def __init__(self, msgid, add, remove, ts: HLC) -> None:
-        self.messageid = msgid
+    def __init__(self, msgid: str, add: dict, remove: dict, ts: str) -> None:
+        self.msg_id = msgid
         self.add = add
         self.remove = remove
         self.ts = ts
