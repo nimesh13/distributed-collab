9a1f58480bc6c21eae2e3d6633bb291e59eb0825
nimesh13ad <nimesh13ad@gmail.com>
2022-11-26 22:15:47 -0800

    connect to node on startup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from uuid import uuid1
 import controller
-from controller import app
+from controller import app, initiateConn
 import argparse
 from hlc import HLC
 from time import time
@@ -10,10 +10,12 @@ from lww import LWW
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', default="0.0.0.0", dest='host', help='Host IP address where this server starts up at.')
 parser.add_argument('--port', default="8002", dest='port', help="Port number where this server starts up at.")
+parser.add_argument('--connect', dest='connect', help='Address of the node you would like to connect to.')
 
 args, _ = parser.parse_known_args()
 port = args.port
 host = args.host
+connect = args.connect
  
 if __name__ == "__main__":
     hlc = HLC(int(time()), str(uuid1()))
@@ -21,4 +23,6 @@ if __name__ == "__main__":
     controller.NODE_URL = "http://" + str(host) + ":" + str(port)   # Makes the node URL consistent
     app.config['clock'] = hlc
     app.config['lww'] = lww
+    if connect:
+        initiateConn(connect)
     app.run(host=host, port=port)
