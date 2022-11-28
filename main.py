c567ee688ec20976ee49d64713be86f909080b13
nimesh13ad <nimesh13ad@gmail.com>
2022-11-27 16:55:34 -0800

    use map to store host port and url

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from uuid import uuid1
-import controller
 from controller import app, initiateConn
 import argparse
 from hlc import HLC
 from time import time
 from lww import LWW
+from constants import NODE
 
 # Parse initialization parameters
 parser = argparse.ArgumentParser()
@@ -13,16 +13,16 @@ parser.add_argument('--port', default="8002", dest='port', help="Port number whe
 parser.add_argument('--connect', dest='connect', help='Address of the node you would like to connect to.')
 
 args, _ = parser.parse_known_args()
-port = args.port
-host = args.host
-connect = args.connect
- 
+NODE['PORT'] = args.port
+NODE['HOST'] = args.host
+NODE['URL'] = "http://" + NODE['HOST'] + ":" + NODE['PORT']
+CONNECT = args.connect
+
 if __name__ == "__main__":
     hlc = HLC(int(time()), str(uuid1()))
     lww = LWW()
-    controller.NODE_URL = "http://" + str(host) + ":" + str(port)   # Makes the node URL consistent
     app.config['clock'] = hlc
     app.config['lww'] = lww
-    if connect:
-        initiateConn(connect)
-    app.run(host=host, port=port)
+    if CONNECT:
+        initiateConn(CONNECT)
+    app.run(host=NODE['HOST'], port=NODE['PORT'])
